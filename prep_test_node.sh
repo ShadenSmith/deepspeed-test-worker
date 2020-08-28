e99699ea1ec5c35cf0d9ee90aa9661e0f17d6f33
Shaden Smith <Shaden.Smith@microsoft.com>
2020-08-28 18:01:24 +0000

    import test script

---
+++ prep_test_node.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+if [[ -z "${DEEPSPEED_PAT}" ]]; then
+    echo "\$DEEPSPEED_PAT must store personal auth token (PAT) for Azure Pipelines agent pool."
+    exit 1
+fi
+
+if [[ -z "${DEEPSPEED_TEST_BASE}" ]]; then
+    export DEEPSPEED_TEST_BASE="/tmp/deepspeed-testing"
+    echo "Using DEEPSPEED_TEST_BASE=${DEEPSPEED_TEST_BASE}"
+fi
+
+#
+# Install Azure Pipelines agent
+#
+
+AGENT_DIR=${DEEPSPEED_TEST_BASE}/agent
+AGENT_VERSION="2.172.2"
+
+mkdir -p ${AGENT_DIR}
+
+pushd ${AGENT_DIR}
+wget https://vstsagentpackage.azureedge.net/agent/${AGENT_VERSION}/vsts-agent-linux-x64-${AGENT_VERSION}.tar.gz -O agent.tar.gz
+tar xf agent.tar.gz
+
+./config.sh \
+    --unattended \
+    --url https://dev.azure.com/DeepSpeedMSFT \
+    --pool DS_testing \
+    --agent ${HOSTNAME} \
+    --replace \
+    --auth pat --token ${DEEPSPEED_PAT}
+
+popd
+
+
+#
+# Install miniconda
+#
+CONDA_INSTALL_PATH=${DEEPSPEED_TEST_BASE}/miniconda3
+
+pushd ${DEEPSPEED_TEST_BASE}
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda-latest.sh
+bash miniconda-latest.sh \
+    -b \
+    -f \
+    -p ${CONDA_INSTALL_PATH}
+
+export PATH="${CONDA_INSTALL_PATH}/bin:${PATH}"
+
+pushd ${AGENT_DIR}
+./run.sh
